e05ff7644507fab87c1c42ff1f727e03e43cc7ec
genki <genki@hagata.info>
2014-04-20 09:10:58 -0700

    Galileo interrupts working counter added non-use timer added

---
+++ HALLInterups_Sensor_A/HALLInterups_Sensor_A.ino
@@ -0,0 +1,59 @@
+
+
+const int hallPin = 12;     // the number of the hall effect sensor pin
+const int ledPin =  13;     // the number of the LED pin
+
+
+int hallState = 0;    // variable for reading the hall sensor status
+
+//variables for reading "start" from MAX
+int serialvalue; // value for serial input
+int started = 0; // flag for whether we've received serial yet
+int countervalue = 0; // counter value
+
+
+void setup() {
+  Serial.begin(9600);
+
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);      
+  // initialize the hall effect sensor pin as an input:
+  pinMode(hallPin, INPUT);     
+}
+
+void loop(){
+
+  if(Serial.available()) // check to see if there's serial data in the buffer
+  {
+    serialvalue = Serial.read(); // read a byte of serial data
+    started = 1; // set the started flag to on
+  }
+
+  // loop once serial data has been received
+  if(started){
+    Serial.print("start");
+    // read the state of the hall effect sensor:
+    hallState = digitalRead(hallPin);
+
+    if (hallState == LOW) {     
+      // turn LED on:    
+      digitalWrite(ledPin, LOW);  
+      Serial.print(hallState);
+      Serial.println();
+    } 
+    else {
+      // turn LED off:
+      digitalWrite(ledPin, HIGH); 
+      Serial.print(hallState);
+      Serial.println();
+
+    }
+
+
+    delay(10);
+  }
+
+
+}
+
+
---
+++ InterruptsTimer_A/InterruptsTimer_A.ino
@@ -0,0 +1,44 @@
+int pin = 13;
+volatile int state = LOW;
+volatile int counter = 0;
+
+unsigned long minute_1 = 60000;
+unsigned long tensec = 10000;
+
+unsigned long usetime;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(pin, OUTPUT);
+  attachInterrupt(2, use, CHANGE);
+}
+
+void loop()
+{
+  unsigned time= millis();
+  Serial.print("timer:");
+  Serial.print("\t");
+  Serial.print(time,10);
+  Serial.print("\t");
+  Serial.print("Counter");
+  Serial.print("\t");
+  Serial.println(counter);
+  delay(1000);
+  digitalWrite(pin, state);
+  
+  if((millis() - usetime) >= tensec){
+   Serial.print("it works"); 
+   counter --;
+   if (counter < 0){counter = 0;}
+  }
+  
+}
+
+void use()
+{
+  usetime = millis();
+  state = !state;
+  counter++;
+}
+
